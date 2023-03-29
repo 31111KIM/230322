d935cf211976e7b61ee94a3c469f75aa977069c7
31111 <128555415+31111KIM@users.noreply.github.com>
2023-03-29 00:19:02 +0000

    and랑 or 표시

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
-/*printf1.c*/
-#include <stdio.h>
+// /*printf1.c*/
+// #include <stdio.h>
 
-int main(void)
-{
-	printf("Hello Everybody \n");
-	printf("%d \n", 1234);
-	printf("%d %d \n", 10, 20);
-	return 0;
-}
+// int main(void)
+// {
+// 	printf("Hello Everybody \n");
+// 	printf("%d \n", 1234);
+// 	printf("%d %d \n", 10, 20);
+// 	return 0;
+// }
