6fc2bc4fb5d040ec141fc085aa4d13062ceca89a
31111 <>
2023-04-06 04:59:14 +0000

    ifelse

---
+++ .ccls-cache/@home@runner@230322/if2.c
@@ -0,0 +1,16 @@
+// #include <stdio.h>
+// main(){
+//   int a=5, b=3;
+
+//   if(5>3){
+//     printf("A\n");
+//     printf("B\n");
+//   }
+//   printf("C\n");
+  
+//   if(5<3){
+//     printf("D\n");
+//     printf("E\n");
+//   }
+//   printf("F\n");
+// }
